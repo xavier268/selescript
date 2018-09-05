449effd3bce8bff9c6e9a49f957b2645d612a558
Xavier <xavier@fed28XPS>
2018-09-05 17:58:47 +0200

    Log in separate files.

--- src/main/resources/rt/run.part.sh
+++ src/main/resources/rt/run.part.sh
@@ -15,34 +15,36 @@
 # First, let's check to see if the executable jar was already compiled.
 # If not found, we will try to compile instead.
 
-echo " Log for the run.sh sript on : " > log.txt
-date >> log.txt
+LOG=Log-$(date).txt
+
+echo "Log for the run.sh sript on : " > "$LOG"
+date >> "$LOG"
 
 if [ ! -f target/$JAR ]; then   
     echo "Compilation is needed."
     echo "Preparing to compile target/$JAR"
-    mvn -version >> log.txt
+    mvn -version >> "$LOG"
     if [ $? -ne 0 ] ; then 
         echo "Cannot compile because could not find maven !"
         echo "Go and install the latest maven version and try again ..."
-	cat log.txt
+	cat "$LOG"
         exit 1
     fi
     mvn clean package -e
     if [ $? -ne 0 ] ; then
         echo "Could not compile, somiting is wrong."
         echo "Go check the messages above and try again ..."
-	cat log.txt
+	cat "$LOG"
         exit 2
 	else 
 	echo "Things look ok."
 	echo "Attempting to run the scrapper ..."
 	echo "Next time you run this cripts, the scrapper will run directly with no other message. You can capture its std output."
-	echo "Remember, the latest error log is log.txt"
+	echo "Remember, the latest error log is $LOG"
 	echo ""
 
     fi
 fi
 
-# Run the scrapper, redirecting debugging/errors to the log.txt file.
-java -jar target/$JAR 2>> log.txt
+# Run the scrapper, redirecting debugging/errors to the $LOG file.
+java -jar target/$JAR 2>> "$LOG"
